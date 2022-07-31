fd2b447ac8c6a468ac5d3af816e24e13746e1edf
cmj-410 <stranger.c.410@gmail.com>
2022-07-31 16:11:00 +0800

    add：sayHi打印

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element-plus'
 import 'normalize.css'
-import '@/styles/index.scss'
+import './styles/index.scss'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
 // 权限控制模块
@@ -12,7 +12,9 @@ import './permission'
 // 更改prototype
 import './prototype'
 // 权限指令
-import useDirectives from '@/directives/index'
+import useDirectives from './directives/index'
+// sayhi
+import './utils/sayHi'
 
 const app = createApp(App)
 installElementPlus(app)
---
+++ src/utils/sayHi.js
@@ -0,0 +1,5 @@
+import config from '../../package.json'
+
+console.log('mj-vue-admin后台系统')
+console.log('%c作者：明见', 'color: rgb(135, 206, 235);')
+console.log(`%c项目地址:${config.url}`, 'color: red;')
